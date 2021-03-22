8ca4af5e5c8edf4cc0c1fd072e1cc4934382f4ea
Your Name <yuri.ouverney@hotmail.com>
2021-03-22 19:43:20 -0300

    update

---
+++ main.py
@@ -0,0 +1,48 @@
+"""Calculadora simples feita usando PySimpleGUI"""
+
+import PySimpleGUI as sg
+
+# Define the window's contents
+layout = [[sg.Text("Digite o primeiro número")],
+          [sg.InputText(key='entrada1')],
+          [sg.Text("Digite o segundo número")],
+          [sg.Input(key='entrada2')],
+          [sg.Text("Digite o tipo de operação (+,-,/,*)")],
+          [sg.Input(key='entrada3')],
+          [sg.Text(size=(40,1), key='saida')],
+          [sg.Button('Ok'), sg.Button('Quit')]]
+
+# Create the window
+window = sg.Window('Calculadora do Yurão', layout)
+
+# Display and interact with the Window using an Event Loop
+while True:
+    try:
+        event, values = window.read()
+        # See if user wants to quit or window was closed
+        if event == sg.WINDOW_CLOSED or event == 'Quit':
+            break
+        # Output a message to the window
+        lista1 = float(values['entrada1'])
+        lista2 = float(values['entrada2'])
+        lista3 = (values['entrada3'])
+        print(type(lista3))
+        if "+" in lista3:
+            print(lista3)
+            window['saida'].update(f'O resultado da conta é: {lista1+lista2}')
+        elif "-" in lista3:
+            window['saida'].update(f'O resultado da conta é: {lista1-lista2}')
+        elif "/" in lista3:
+            window['saida'].update(f'O resultado da conta é: {lista1/lista2}')
+        elif "*" in lista3:
+            window['saida'].update(f'O resultado da conta é: {lista1*lista2}')
+        elif "+" or "-" or "/" or "*" not in lista3:
+            window['saida'].update('você deve digitar o tipo de operação que deseja!')
+            continue
+
+    except ValueError:
+        window['saida'].update('você deve digitar apenas números')
+        continue
+
+# Finish up by removing from the screen
+window.close()
